5d708503701f3965b4cc3749381c3cea19d1bcd6
zavodelevator <68054345+zavodelevator@users.noreply.github.com>
2023-10-09 17:01:50 +0300

    form_build

--- busket/calcs/form_build.rb
+++ busket/calcs/form_build.rb
@@ -36,16 +36,20 @@
 #     }
 # }
 aa = {
+  'depiction': ['', ''],
   'id': ['id', ''],
   'name': ['Назва', ''],
   'description': ['Опис', ''],
   'kof_markup': ['Коф націнки', ''],
-  'price_$': ['Ціна', ''],
+  'price_$': ['Ціна $', ''],
+  'price_₴': ['Ціна ₴', ''],
   'personal_data': {
+    'depiction': ['', ''],
     'area': ['Площа покриття', ''],
     'metal_thickness': ['Товщина металу  (мм)', ''],
   },
   'dimensions': {
+    'depiction': ['', ''],
     'height': ['Висота', ''],
     'length': ['Дожина, Вильот', ''],
     'width': ['Ширина, Габарит', ''],
@@ -58,6 +62,7 @@ aa = {
   'links_drawing': ['Креслення', []],
   'producer': ['Виробник, Постачальник', []],
   'shipping_dimensions': {
+    'depiction': ['', ''],
     'height': ['Висота', ''],
     'width': ['Ширина', ''],
     'length': ['Довжина', ''],
@@ -68,6 +73,27 @@ aa = {
   'group_crm': ['Группа црм', ''],
   'sten_on': ['Встановлюється на', ''],
   'consists_of': ['складається з', ''],
-  'other_data': {}
+  'other_data': {
+    'depiction': ['', ''],
+  }
 }
-puts aa.keys
+
+def form_build(a)
+  aanny(a)
+end
+
+def aanny(el)
+  el.each do |key, value|
+    if value.is_a?(Array) && key != 'depiction' && !value.is_a?(Hash) //problemm
+      if value[1].is_a?(Array)
+        puts "input many elements: #{value[0]}"
+      else
+        puts "input one element: #{value[0]}"
+      end
+    elsif value.is_a?(Hash)
+      aanny(value)
+    end
+  end
+end
+
+form_build(aa)
