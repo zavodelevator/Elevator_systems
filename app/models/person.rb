c50e770b3f426a7fa1d6c4245709fe6316dd6915
MishaDeineko1990 <40040773+MishaDeineko1990@users.noreply.github.com>
2023-10-09 22:06:25 +0300

    autorization

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,6 @@
 class Person < ApplicationRecord
   belongs_to :firm
+
+  validates :last_name, presence: true
+  validates :father_name, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_08_071025) do
+ActiveRecord::Schema.define(version: 2023_10_09_185652) do
 
   create_table "firms", force: :cascade do |t|
     t.string "name"
@@ -33,6 +33,18 @@ ActiveRecord::Schema.define(version: 2023_10_08_071025) do
     t.string "last_name"
     t.string "father_name"
     t.index ["firm_id"], name: "index_people_on_firm_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "people", "firms"
